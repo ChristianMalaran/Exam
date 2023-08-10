bccfd49e4d0ad862e648f602e6100f83697ff8f6
kit <kit@gmail.com>
2023-08-10 21:35:08 +0800

    update

---
+++ Tables/employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[employee] (
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
+    [firstname] NCHAR (30) NULL,
+    [middlename] NCHAR (30) NULL,
+    [lastname]  CHAR (30)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
